ad41aee03bb64a90120eeb88d88b94700137ae0e
fillipeoa <fillipeoa1234@gmail.com>
2025-05-05 19:58:30 -0300

    5. Classe TestSuite

--- testing_framework.py
+++ testing_framework.py
@@ -77,3 +77,15 @@ class TestSpy(TestCase):
     def tear_down(self):
         self.was_tear_down = True
         self.log += "tear_down"
+
+class TestSuite:
+
+    def __init__(self):
+        self.tests = []
+
+    def add_test(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
